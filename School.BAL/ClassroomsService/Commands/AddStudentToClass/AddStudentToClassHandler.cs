2c6290348f938088ba8d56982f026aa76dba8093
MIshaPuch <mpuczkowskij@gmail.com>
2025-11-18 16:35:03 +0100

    fix the business logi with 20 students

--- School.BAL/ClassroomsService/Commands/AddStudentToClass/AddStudentToClassHandler.cs
+++ School.BAL/ClassroomsService/Commands/AddStudentToClass/AddStudentToClassHandler.cs
@@ -22,6 +22,12 @@ namespace School.BAL.ClassroomsServices.Commands.AddStudentToClass
             var student = await _studentRepo.GetByIdAsync(request.StudentId, ct)
                           ?? throw new InvalidOperationException("Student not found");
 
+            var classroom = await _classRepo.GetByIdAsync(request.ClassroomId, ct)
+                          ?? throw new InvalidOperationException("Class not found");
+
+            if (classroom.Students.Count >= 20)
+                throw new InvalidOperationException("Class can contain only 20 students.");
+
             await _classRepo.AddStudentAsync(request.ClassroomId, student, ct);
             return Unit.Value;
         }
